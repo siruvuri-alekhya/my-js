0d23998e7315229423872e02631c6b78e9b5c489
Siruvuri, Alekhya <siruvuri.alekhya09@gmail.com>
2016-02-23 21:19:57 -0800

    Played with variable types in javascript

---
+++ section3-13.js
@@ -0,0 +1,17 @@
+/*Ways to create different types of data in js*/
+var numberVar = 1000;
+
+console.log("Printing the typeof variable numberVar with value 1000: " + typeof numberVar);
+
+//Re using an existingvariable which is declared before.
+numberVar = 10000.00;
+
+console.log("Printing the typeof variable numberVar with value 1000.00: " + typeof numberVar);
+
+var stringVar = 'My First String Variable';
+
+console.log("Printing the typeof variable stringVar: " + typeof stringVar);
+
+var undefinedVar;
+
+console.log("printing the typeof variable undefinedVar: " + typeof undefinedVar);
