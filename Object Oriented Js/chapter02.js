e818e10b9cc462a1fa2a8fbf43b44ac30320549d
Alekhya Siruvuri <asiruvuri@Sanjays-MacBook-Air.local>
2016-02-24 22:31:21 -0500

    Objected oriented js chap 2 work

---
+++ Object Oriented Js/chapter02.js
@@ -0,0 +1,44 @@
+
+// In js, variable names can be created using numbers, letters, and _ character or even $ character.
+
+var camelCaseWay;
+var most_usual_way;
+//var _variableWay; // uncommon way - used only in libraries
+
+//invalid variable name
+//var 4VariableName; //This variable is not accepted by js
+
+/*--------------------*/
+// Two ways of assigning a value to a variable
+
+var firstWayVar = "Hello Alekhya";
+
+var secondWayVar;
+
+secondWayVar = "Hello again Alekhya";
+
+//variables are case sensitive
+
+var anApple; // camel case
+var AnApple;
+var ANAPPLE; // CONSTANT Variable declarations
+
+
+var commaSeparatedVar1, commaSeparatedVar2, commaSeparatedVar3;
+
+var commaSeparatedVar1 = 1, commaSeparatedVar2 = 2, commaSeparatedVar3 = 3;
+
+var commaSeparatedVar1 = 'undefined', commaSeparatedVar2, commaSeparatedVar3;
+
+/*--------------------*/
+// VERY VERY IMPORTANT NOTE
+// javascript executes in two phases:
+// - Creation phase
+// - execution phase
+/*--------------------*/
+
+// Please work on exercises from page no - 36
+
+// Continue until page no 59 //end of array section.
+//we will begin conditions and loops
+
