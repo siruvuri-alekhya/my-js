2f2e6e118e5c4d133bc4564cd2e20b7ad0b6cdb8
Alekhya Siruvuri <asiruvuri@Sanjays-MacBook-Air.local>
2016-03-03 19:45:40 -0500

    Finished practicing chapter 02

--- Object Oriented Js/chapter02.js
+++ Object Oriented Js/chapter02.js
@@ -42,3 +42,131 @@ var commaSeparatedVar1 = 'undefined', commaSeparatedVar2, commaSeparatedVar3;
 // Continue until page no 59 //end of array section.
 //we will begin conditions and loops
 
+//Practise by Alekhya
+
+
+var a;
+var isNum;
+var ISNUM;
+var _this_is_num;
+//the variable should not start with numbers.
+
+
+
+//Initialising a variable
+
+var apple=6;
+var a,b,c;
+var a=1, b=3, d=4;
+var l, bm, n=70, i='hello',f;
+
+//operators
+var a=20;
+var b=30;
+var c=a+b;
+var d= a+20;
+
+//arithmetic operators
+/*
+addition '+'
+subtraction'-';
+multiplication '*';
+division'/';
+modulo'%';
+increment '++';
+decrement '--';
+*/
+//compound operators
+
+var a=60;
+a += 30;
+a -=10;
+a *= 4;
+a /=6;
+
+//primitive datatypes;
+
+
+//If looops
+/*
+var input = window.prompt("Please enter your name", "Who is this?");
+
+if(input === "alekhya"){
+    console.log("Yes this is alekhya");    
+}else if(input === "sanjay"){
+    console.log("Yes this is sanjay");    
+}else if(input === "guest"){
+    console.log("Yes this is a guest");    
+}else{
+    console.log("NO this is nobody");    
+}
+*/
+/*
+//Boolean operators
+&&
+||
+!=
+!==
+!==
+==
+===
+
+Output of a boolean operation = Boolean type of output
+
+
+//Arithmetic operators
++ - / *
+
+
+
+//Logical operators
+|
+&
+//HAS TO BE A NUMBER - check the truth table
+
+//Assignment operators
+= 
+*/
+
+//var v = 100;
+
+var v = null;
+var v = v || 10;
+
+// true || false = true
+// false || true = true
+// true || true = true
+// false || false = false
+
+console.log("value of v : "+v);
+
+
+
+
+//multiplication program
+
+
+
+
+var total;
+for(i=0;i<4;i++)
+    {
+      for(j=0;j<10;j++)
+      {
+          total=(i*j);
+          total=total+"";
+          console.log(total);
+    }
+        console.log("<br>");
+    }
+
+
+var a=10;
+var b=20;
+
+function sum(a,b)
+{
+    var c= a+b;
+    return c;
+}
+
